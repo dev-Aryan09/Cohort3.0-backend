5ebbf28660c98c164e34e7ca4ec969b98b1477fc
Aryan Saini <09aryan.lucky@gmail.com>
2026-08-25 22:14:55 +0530

    install dotenv and create .env file

--- day6-notesApp/server.js
+++ day6-notesApp/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
 });
--- day6-notesApp/src/config/db.js
+++ day6-notesApp/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/notes-app");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("DB connected successfully");
   } catch (error) {
     console.log("Error while connecting DB", error);
