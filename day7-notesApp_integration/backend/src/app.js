60812bcba2284e2fbbdd13527851fd682c3544db
Aryan Saini <09aryan.lucky@gmail.com>
2026-09-01 11:32:11 +0530

    enable CORS

--- day7-notesApp_integration/backend/src/app.js
+++ day7-notesApp_integration/backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const connectDB = require("./config/db");
 
@@ -7,6 +8,11 @@ const notesRoute = require("./routes/notes.route");
 connectDB();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 // app.post("/create", createNotesController);
 // app.get("/", getAllNotesController);
