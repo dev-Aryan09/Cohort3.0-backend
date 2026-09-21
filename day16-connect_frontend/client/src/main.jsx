7d11d0f985ad94a8a47e39f726d4f037cf79cd3d
Aryan Saini <09aryan.lucky@gmail.com>
2026-09-22 01:02:00 +0530

    create useAuthContext hook in the context file

--- day16-connect_frontend/client/src/main.jsx
+++ day16-connect_frontend/client/src/main.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import {
+  AuthContext,
+  AuthProvider,
+} from "./modules/auth/context/AuthContext.jsx";
 
-createRoot(document.getElementById("root")).render(<AppRoutes />);
+createRoot(document.getElementById("root")).render(
+  <AuthProvider value={AuthContext}>
+    <AppRoutes />
+  </AuthProvider>,
+);
--- day16-connect_frontend/client/src/modules/auth/context/AuthContext.jsx
+++ day16-connect_frontend/client/src/modules/auth/context/AuthContext.jsx
@@ -1,13 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-export const AuthStore = createContext();
+export const AuthContext = createContext();
+
+/*
+  custom hook,
+  use this for using state in other components
+ */
+export function useAuthContext() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuthContext must be used within an AuthProvider");
+  }
+
+  return context;
+}
 
 export const AuthProvider = ({ children }) => {
   console.log("Auth context rendering...");
   const [user, setUser] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
 
-  <AuthStore.Provider value={{ user, setUser, accessToken, setAccessToken }}>
-    {children}
-  </AuthStore.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{ user, setUser, accessToken, setAccessToken }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
