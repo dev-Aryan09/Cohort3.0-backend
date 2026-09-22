e9e4760f26ded50440013e142886140b2fec3512
Aryan Saini <09aryan.lucky@gmail.com>
2026-09-22 21:48:54 +0530

    set-up vite server proxy configuration

--- day16-connect_frontend/client/vite.config.js
+++ day16-connect_frontend/client/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // vite server proxy configuration
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
